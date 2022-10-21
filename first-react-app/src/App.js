794af336438e71490cda8a66b98e2df73b4a84d7
Pedro López <plopez@financialforce.com>
2022-10-21 09:47:14 +0200

    Lesson 17

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,26 +1,17 @@
-import logo from './assets/logo.svg';
 import './App.css';
 
 function App() {
+  let name = 'Mario'
+
+  const updateName = () => {
+    name = 'Luigi';
+    console.log(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          I'm here already
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>My name is: {name}</p>
+      <button onClick={updateName}>Change my name</button>
     </div>
   );
 }
