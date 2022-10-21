18062e0ae28810652c6472d67aa0198e861a026c
Pedro López <plopez@financialforce.com>
2022-10-21 11:20:44 +0200

    Lesson 21

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,25 +2,27 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('Mario')
   const [events, setEvents] = useState([
     { id: 1, title: 'Im Mario!!!!' },
     { id: 2, title: 'Im Luigi!!!!' },
     { id: 3, title: 'Im Peach!!!!' }
   ])
 
-  const updateName = () => {
-    setName('Luigi');  
+  const handleClick = (clickedEventId) => {
+    console.log('clicked Event', clickedEventId);
+    setEvents((previousEvents) => {
+      return previousEvents.filter((event) => {
+        return event.id !== clickedEventId
+      })
+    })
   }
 
   return (
     <div className="App">
-      <p>My name is: {name}</p>
-      <button onClick={updateName}>Change name</button>
-
       { events.map((event, mapIndex) => (
         <div key={event.id} >
           <h2>{mapIndex} - { event.title }</h2>
+          <button onClick={ ()=>{handleClick(event.id)} }>Delete Event</button>
         </div>
       )) }
     </div>
