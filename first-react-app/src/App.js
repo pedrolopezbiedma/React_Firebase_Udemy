1ae47a4bc4dad400eb100b2c715afc3854591ec7
Pedro López <plopez@financialforce.com>
2022-10-21 10:17:11 +0200

    Lesson 20

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState('Mario')
+  const [events, setEvents] = useState([
+    { id: 1, title: 'Im Mario!!!!' },
+    { id: 2, title: 'Im Luigi!!!!' },
+    { id: 3, title: 'Im Peach!!!!' }
+  ])
 
   const updateName = () => {
-    setName('Luigi');
-    console.log(name);
+    setName('Luigi');  
   }
 
   return (
     <div className="App">
       <p>My name is: {name}</p>
       <button onClick={updateName}>Change name</button>
+
+      { events.map((event, mapIndex) => (
+        <div key={event.id} >
+          <h2>{mapIndex} - { event.title }</h2>
+        </div>
+      )) }
     </div>
   );
 }
