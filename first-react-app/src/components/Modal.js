b3c5c5ebadc35f965e115c02306544c3958fb208
Pedro López <plopez@financialforce.com>
2022-10-26 16:07:43 +0200

    Lesson 36

--- first-react-app/src/components/Modal.js
+++ first-react-app/src/components/Modal.js
@@ -3,7 +3,11 @@ import './Modal.css';
 export default function Modal({children, toggleModal}) {
   return (
     <div className='modal-backdrop'>
-        <div className="modal">
+        <div className="modal" style={{
+            border: "4px solid",
+            borderColor: "#ff4500",
+            textAlign:"center"
+          }}>
             {children}
             <button onClick={toggleModal} >Close</button>
         </div>
