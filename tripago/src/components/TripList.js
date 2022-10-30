89cf90d2ba2b2e0af52bc8d67fa6d74c32ba9a4a
Pedro López <plopez@financialforce.com>
2022-10-30 18:51:27 +0100

    Lesson 53

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,29 +1,17 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
 import './TripList.css'
 
 export default function TripList() {
     const [url, setUrl] = useState('http://localhost:3000/trips')
-    const [trips, setTrips] = useState([])
-
-    const fetchTrips = useCallback(
-      async () => {
-        const response = await fetch(url);
-        const trips = await response.json();
-        setTrips(trips)
-      },
-      [url]
-    )
-
-    useEffect(() => {
-        fetchTrips()
-    }, [fetchTrips])
+    const { data: trips } = useFetch(url);
 
     return (
         <div className='trip-list'>
             <h2>TripList</h2>
             
             <ul>
-                { trips.map((trip) => ( 
+                { trips && trips.map((trip) => ( 
                     <li key={trip.id}>
                         <h3>{ trip.title }</h3>
                         <p>{ trip.price }</p>
