fbc2a54a2b799f55c5a5818b691d2b265defdbcb
Pedro López <plopez@financialforce.com>
2022-10-30 11:44:07 +0100

    Lesosn 49

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import './TripList.css'
 
 export default function TripList() {
-
     const [trips, setTrips] = useState([])
 
-    fetch('http://localhost:3000/trips')
-        .then(response => response.json())
-        .then(    console.log(trips))
+    useEffect(() => {
+        fetch('http://localhost:3000/trips')
+            .then(response => response.json())
+            .then(trips => setTrips(trips))
+    }, [])
 
     return (
         <div>
             <h2>TripList</h2>
+            
+            <ul className='trip-list'>
+                { trips.map((trip) => ( 
+                    <li key={trip.id}>
+                        <h3>{ trip.title }</h3>
+                        <p>{ trip.price }</p>
+                    </li>
+                )) }
+            </ul>
         </div>
     )
 }
