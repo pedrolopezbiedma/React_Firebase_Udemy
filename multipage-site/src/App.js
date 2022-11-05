21840ecff566fa1b869e41496fecf1dc7f243d28
Pedro López <plopez@financialforce.com>
2022-11-05 17:44:25 +0100

    Lesson 70

--- multipage-site/src/App.js
+++ multipage-site/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-          <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+            <h1>My Articles</h1>
+        </nav>
+
+        <Route path='/'>
+        <Home />
+      </Route>
+      <Route path='/contact'>
+        <Contact />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ multipage-site/src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+        <h2>Home</h2>
+        <p>lorem ipsum Home</p>
+    </div>
+  )
+}
