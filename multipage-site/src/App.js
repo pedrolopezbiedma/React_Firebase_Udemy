52da3efcfbdfa0d2bfca3491f13b4aade1d4951a
Pedro López <plopez@financialforce.com>
2022-11-06 08:40:18 +0100

    Lesson 73

--- multipage-site/src/App.js
+++ multipage-site/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
--- multipage-site/src/pages/Home.js
+++ multipage-site/src/pages/Home.js
@@ -1,8 +1,21 @@
+import { useFetch } from '../hooks/useFetch'
+
+import './Home.css'
+
 export default function Home() {
+
+  const { data: articles, isLoading, error } = useFetch('http://localhost:3000/articles');
+
   return (
-    <div>
-        <h2>Home</h2>
-        <p>lorem ipsum Home</p>
+    <div className='home'>
+      { isLoading && ( <div>is loading...</div>)}
+      { error && ( <div>{error}</div>)}
+      { articles && articles.map((article) => (
+        <div key={article.id} className='card'>
+          <h2>{article.title}</h2>
+          <p>{article.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
