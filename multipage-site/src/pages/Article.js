a7e6c3fb66b75948580d210f00303f863601d6d8
Pedro López <plopez@financialforce.com>
2022-11-06 09:18:22 +0100

    Lesson 76

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,11 +1,21 @@
-import { useParams } from 'react-router-dom' 
+import { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom' 
 import { useFetch } from '../hooks/useFetch'
 
 export default function Article() {
     const { id } = useParams();
+    const history = useHistory();
     const url = 'http://localhost:3000/articles/' + id;
     const { data: article, isLoading, error } = useFetch(url)
 
+    useEffect(() => {
+        if(error){
+            setTimeout(() => {
+                history.push('/')
+            }, 2000);
+        }
+    }, [error, history])
+    
     return (
         <div>
             { isLoading && ( <div>is loading...</div>)}
