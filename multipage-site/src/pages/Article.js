8d0c8f06e6d6b3aeb896cfe1c30049fbfb41945f
Pedro López <plopez@financialforce.com>
2022-11-06 09:06:50 +0100

    Lesson 75

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,5 +1,22 @@
+import { useParams } from 'react-router-dom' 
+import { useFetch } from '../hooks/useFetch'
+
 export default function Article() {
-  return (
-    <div>Article page</div>
-  )
+    const { id } = useParams();
+    const url = 'http://localhost:3000/articles/' + id;
+    const { data: article, isLoading, error } = useFetch(url)
+
+    return (
+        <div>
+            { isLoading && ( <div>is loading...</div>)}
+            { error && ( <div>{error}</div>)}
+            { article && (
+                <div>
+                    <h2>{article.title}</h2>
+                    <p>{article.author}</p>
+                    <p>{article.body}</p>
+                </div>
+            )}
+        </div>
+    )
 }
