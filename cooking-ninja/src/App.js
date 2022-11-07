761cb019bc2337d2fc634364a71f0d7942c7cec3
Pedro López <plopez@financialforce.com>
2022-11-07 15:56:32 +0100

    Lesson 80

---
+++ cooking-ninja/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+import Create from './pages/Create/Create'
+import Search from './pages/Search/Search'
+import Recipe from './pages/Recipe/Recipe'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/recipe/:id'>
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
