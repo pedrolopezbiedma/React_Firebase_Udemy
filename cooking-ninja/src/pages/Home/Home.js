22770477ceb2adcfb99fff478bdad7f9b8b3276d
Pedro López <plopez@financialforce.com>
2022-11-07 16:38:48 +0100

    Lesson 82

--- cooking-ninja/src/pages/Home/Home.js
+++ cooking-ninja/src/pages/Home/Home.js
@@ -1,7 +1,18 @@
+import { useFetch } from '../../hooks/useFetch'
+
 import './Home.css'
 
 export default function Home() {
+const { data: recipes, isLoading, error } = useFetch('http://localhost:3000/recipes')
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      { isLoading && ( <p className='loading'>Loading...</p>) }
+      { error && ( <p className='error'>{ error }</p>) }
+      
+      { recipes && recipes.map((recipe) => (
+        <h2>{ recipe.title }</h2>
+      ))}
+    </div>
   )
 }
