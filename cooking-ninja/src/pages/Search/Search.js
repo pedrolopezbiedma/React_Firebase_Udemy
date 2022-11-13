049ff01bb5aaf80da48900f90593a468cd8ad0a7
Pedro López <plopez@financialforce.com>
2022-11-13 19:33:48 +0100

    Lesson 91

--- cooking-ninja/src/pages/Search/Search.js
+++ cooking-ninja/src/pages/Search/Search.js
@@ -1,7 +1,25 @@
+import { useLocation } from 'react-router-dom'
+import RecipeList from '../../components/RecipeList'
+
+import { useFetch } from '../../hooks/useFetch'
+
 import './Search.css'
 
 export default function Search() {
+  const location = useLocation().search
+  const term = new URLSearchParams(location).get('query')
+
+  const { data: filteredRecipes, isLoading, error } = useFetch('http://localhost:3000/recipes?q=' + term )
+
   return (
-    <div>Search</div>
+    <div>
+      <h2 className='page-title'>Recipes including "{term}"</h2>
+      { isLoading && ( <p className='loading'>Loading...</p>) }
+      { error && ( <p className='error'>{ error }</p>) }
+
+      { filteredRecipes && ( 
+        <RecipeList recipes={filteredRecipes} />
+      )}
+    </div>
   )
 }
