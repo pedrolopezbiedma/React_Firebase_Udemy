e9a7d86ea2fb749e8d30babbc64e0435f75bb8d9
Pedro López <plopez@financialforce.com>
2022-11-14 12:47:02 +0100

    Cleaning all to practice

--- cooking-ninja/src/components/Navbar.js
+++ cooking-ninja/src/components/Navbar.js
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom'
-import { useThemeContext } from '../hooks/useThemeContext'
 
 import SearchBar from './SearchBar'
 import './Navbar.css'
 
 export default function Navbar() {
-  const { color } = useThemeContext();
 
   return (
-    <div className='navbar' style={{ background: color }}>
+    <div className='navbar'>
         <nav>
             <Link to='/' className='brand'>
                 <h1>Cooking Ninja</h1>
--- cooking-ninja/src/index.js
+++ cooking-ninja/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import ThemeProvider from './context/ThemeContext';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ThemeProvider>
         <App />
-    </ThemeProvider>
 );
 
