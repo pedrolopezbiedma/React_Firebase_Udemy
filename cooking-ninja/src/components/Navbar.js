fbeb6871be781fdb771be56ecd7d73be9273e23c
Pedro López <plopez@financialforce.com>
2022-11-14 08:55:29 +0100

    Lesson 96

--- cooking-ninja/src/components/Navbar.js
+++ cooking-ninja/src/components/Navbar.js
@@ -1,11 +1,15 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 import { Link } from 'react-router-dom'
 
 import SearchBar from './SearchBar'
 import './Navbar.css'
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={ {background: color} }>
         <nav>
             <Link to='/' className='brand'>
                 <h1>Cooking Ninja</h1>
