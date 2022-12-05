01bb583145fcd5fcb4a017e92199c43a96b39a57
Pedro López <plopez@financialforce.com>
2022-12-05 15:51:32 +0100

    Lesson 115 & 116

--- my-money/src/pages/login/Login.js
+++ my-money/src/pages/login/Login.js
@@ -1,7 +1,28 @@
+import { useState } from 'react'
+
 import styles from './Login.module.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('State es ->', email + ' , ' + password )
+  }
+
   return (
-    <div>Login</div>
+    <form className={styles['login-form']} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email: </span>
+        <input type='email' onChange={(event) => setEmail(event.target.value)}/>
+      </label>
+      <label>
+        <span>Password: </span>
+        <input type='password' onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
   )
 }
--- my-money/src/pages/signup/Signup.js
+++ my-money/src/pages/signup/Signup.js
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+
 import styles from './Signup.module.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Event es --> ',email + ' , ' + password + ' , ' + displayName);
+  }
+
   return (
-    <div>Signup</div>
+    <form className={styles['signup-form']} onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email: </span>
+        <input type='email' onChange={(event) => setEmail(event.target.value)}/>
+      </label>
+      <label>
+        <span>Password: </span>
+        <input type='password' onChange={(event) => setPassword(event.target.value)}/>
+      </label>
+      <label>
+        <span>Display Name: </span>
+        <input type='text' onChange={(event) => setDisplayName(event.target.value)}/>
+      </label>
+      <button className='btn'>Sign Up</button>
+    </form>
   )
 }
