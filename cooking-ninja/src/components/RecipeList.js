60b59c68909d3ada2866a55540e8300dc46e8cb8
Pedro López <plopez@financialforce.com>
2022-12-05 10:46:35 +0100

    Lesson 108

--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -1,10 +1,17 @@
+import { projectFirestore } from '../firebase/config'
 import { Link } from 'react-router-dom'
 import { useThemeContext } from '../hooks/useThemeContext'
+import trashcan from '../assets/trashcan.svg'
+
 import './RecipeList.css'
 
 export default function RecipeList({ recipes }) {
   const { mode } = useThemeContext()
 
+  const handleDelete = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete();
+
+  }
   return (
     <div className='recipe-list'>
         { recipes.map((recipe) => (
@@ -13,6 +20,12 @@ export default function RecipeList({ recipes }) {
                 <p>{ recipe.cookingTime } to make</p>
                 <div>{ recipe.method.substring(0,100) + '...' }</div>
                 <Link to={`/recipe/${recipe.id}`}>Cook this</Link>
+                <img 
+                  className='delete'
+                  alt='delete icon'
+                  src={trashcan}
+                  onClick={() => handleDelete(recipe.id)}
+                />
             </div>
         )) }
     </div>
