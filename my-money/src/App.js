381cc2e18b2abfd4f122e2aae545fee0d38880e9
Pedro López <plopez@financialforce.com>
2022-12-05 15:15:56 +0100

    Lesson 114

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/home'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
