0b898a19e1a49d691f0d746f8d5a36153f9c08c0
Pedro López <plopez@financialforce.com>
2022-12-06 08:32:39 +0100

    Lesson 117

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAn8sKWIyCpJQvR-uEODgqNOi02eVWuWQc",
+    authDomain: "mymoney-ninja-site-fd42d.firebaseapp.com",
+    projectId: "mymoney-ninja-site-fd42d",
+    storageBucket: "mymoney-ninja-site-fd42d.appspot.com",
+    messagingSenderId: "1091772104659",
+    appId: "1:1091772104659:web:15b54ce5841d84cc30b926"
+  };
+
+// Firebase initialization
+  firebase.initializeApp(firebaseConfig);
+
+// Firestore database initializacion
+  const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
